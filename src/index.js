9d5026aa980469acb6f1cc2f050aacf00a93055d
MikhailPilipenko <pmgomel@mail.ru>
2021-03-07 21:18:09 +0300

    fead: add function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  //Array.prototype.map()
+  //create a new array of strings
+  let arrBrackets = bracketsConfig.map((item) => item.join(""));
+ 
+    for ( let i = 0; i < arrBrackets.length; i++) {
+	  //String.prototype.includes()
+	  if (str.includes(arrBrackets[i])) {
+		  //String.prototype.replace()
+		  str = str.replace(arrBrackets[i], "");
+		  //start the loop again
+		  i = -1;
+	  }
+  }
+  
+  return !str;
+  
 }
